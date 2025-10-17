89f05a1ff07b1ec7c1368b814bbe9a59c7634ca8
George Mwangi Gichuru <georgem.gichuru@gmail.com>
2025-10-17 11:42:15 +0300

    Reveerto a previous commit

--- fix_indent.py
+++
@@ -1,4 +0,0 @@
-with open('app/payments/views.py', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-with open('app/payments/views.py', 'w', encoding='utf-8') as f:
-    f.writelines([line[1:] if line.startswith(' ') else line for line in lines])
